20d896ebf709790ecafabd510500beb6268cc01b
kamsi okonji <kamsi.okonji9@gmail.com>
2025-02-08 10:06:09 +0100

    feat: add more color customization and upgrade tailwind to version 4

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,27 @@ function App() {
   console.log(data);
 
   return (
-    <>
-      <div className="text-3xl">Classic Tiramisu</div>
-    </>
+    <div className={"flex flex-row px-8 py-10 gap-4"}>
+      <span className={"grid grid-cols-3 gap-9"}>
+        <p className={"p-9"}>hello</p>
+        <p className={"p-9"}>hello</p>
+        <p className={"p-9"}>hello</p>
+        <p className={"p-9"}>hello</p>
+        <p className={"p-9"}>hello</p>
+        <p className={"p-9"}>hello</p>
+        <p className={"p-9"}>hello</p>
+        <p className={"p-9"}>hello</p>
+        <p className={"p-9"}>hello</p>
+      </span>
+
+      <div className={"flex-1"}>
+        <div className="font-bold text-primary-foreground">
+          Classic Tiramisu
+        </div>
+
+        <div>good afternoon</div>
+      </div>
+    </div>
   );
 }
 
